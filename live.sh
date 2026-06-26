5d513dc4d7f3bc8483d949b3e9d3e8bf045789d5
karolgotow <karol.gotowala@proton.me>
2026-06-26 11:21:44 +0000

    Update live.sh

--- live.sh
+++ live.sh
@@ -6,8 +6,9 @@ DESKTOP_FOCUSED=0
 DESKTOP_WAS_FOCUSED=0
 LIVE_WALLPAPER_PROGRAM_PID=0
 LIVE_WALLPAPER_PROGRAM_WID=0
-CURRENT_WORKSPACE=0
-REPLY_GLOBAL=""
+REPLY_GLOBAL="NORMAL"
+LIVE_FOCUSED=0
+COUNT=0
 
 function QUIT_FUNC() {
 	NORMAL_WALLPAPER
@@ -21,6 +22,9 @@ function QUIT_FUNC() {
 
 	"
 	REPLY_GLOBAL=$REPLY
+	if ! [[ $REPLY_GLOBAL == "NORMAL" || $REPLY_GLOBAL == "QUIT" ]]; then
+	LIVE_WALLPAPER
+	fi
 }
 
 function NORMAL_WALLPAPER() {
@@ -34,11 +38,43 @@ function NORMAL_WALLPAPER() {
 		wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b remove,sticky
 }
 
+function LIVE_WALLPAPER() {
+	LIVE_WALLPAPER_PROGRAM_PID=$REPLY_GLOBAL
+	LIVE_WALLPAPER_PROGRAM_WID=$( wmctrl -lp | grep $LIVE_WALLPAPER_PROGRAM_PID | awk 'NR==1{print $1}' )
+	DESKTOP_WID=$(wmctrl -l -x -G | grep xfceliveDesktop | awk 'NR==1{print $1}')
+	wmctrl -r "xfceliveDesktop" -b add,sticky
+	wmctrl -r "xfceliveDesktop" -b add,hidden
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -e '0, 0, 0, -1, -1'
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,maximized_vert
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,maximized_horz
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,sticky
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,below
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,skip_pager
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,skip_taskbar
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b remove,hidden
+
+	#those fors for settings are borrowed from xfce forum post here @ ToZ
+	#https://forum.xfce.org/viewtopic.php?pid=73547#p73547
+	for x in $(xfconf-query -c xfce4-desktop -lv | grep color-style | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $x -s "3"; done
+	for x in $(xfconf-query -c xfce4-desktop -lv | grep image-style | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $x -s "0"; done
+	for x in $(xfconf-query -c xfce4-desktop -lv | grep single-workspace-mode | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $x -s "false"; done
+	for x in $(xfconf-query -c xfce4-desktop -lv | grep single-workspace-mode | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $x -s "true"; done
+
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,above
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b remove,below
+	wmctrl -r "xfceliveDesktop" -b add,above
+	wmctrl -r "xfceliveDesktop" -b remove,below
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b remove,above
+	wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,below
+	wmctrl -r "xfceliveDesktop" -b remove,above
+	wmctrl -r "xfceliveDesktop" -b add,below
+}
+
 trap "QUIT_FUNC" SIGINT
 clear
 echo "CTRL+C to get prompt"
 while true; do
-	sleep 0.04
+	sleep 0.1
 	wait $( wmctrl -r "xfceliveDesktop" -e '0, 0, 0, 0, 0' )
 
 	if [[ $REPLY_GLOBAL == "QUIT" ]]; then
@@ -47,59 +83,77 @@ while true; do
 
 	elif  [[ $REPLY_GLOBAL == "NORMAL" ]]; then
 		NORMAL_WALLPAPER
-		while [[ $REPLY_GLOBAL == "NORMAL" ]]; do
-			sleep 0.2
-			wait $( wmctrl -r "xfceliveDesktop" -e '0, 0, 0, 0, 0' )
-		done
-	elif ! [[ $REPLY_GLOBAL == "" ]]; then
-		LIVE_WALLPAPER_PROGRAM_PID=$REPLY_GLOBAL
-		LIVE_WALLPAPER_PROGRAM_WID=$( wmctrl -lp | grep $LIVE_WALLPAPER_PROGRAM_PID | awk 'NR==1{print $1}' )
 		DESKTOP_WID=$(wmctrl -l -x -G | grep xfceliveDesktop | awk 'NR==1{print $1}')
-		wmctrl -r "xfceliveDesktop" -b add,sticky
-		wmctrl -r "xfceliveDesktop" -b add,hidden
-		wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -e '0, 0, 0, -1, -1'
-		wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,maximized_vert
-		wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,maximized_horz
-		wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,sticky
-		wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,below
-		wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,skip_pager
-		wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,skip_taskbar
-		wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b remove,hidden
-
-		#those fors for settings are borrowed from xfce forum post here @ ToZ
-		#https://forum.xfce.org/viewtopic.php?pid=73547#p73547
-		for x in $(xfconf-query -c xfce4-desktop -lv | grep color-style | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $x -s "3"; done
-		for x in $(xfconf-query -c xfce4-desktop -lv | grep image-style | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $x -s "0"; done
-		for x in $(xfconf-query -c xfce4-desktop -lv | grep single-workspace-mode | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $x -s "false"; done
-		for x in $(xfconf-query -c xfce4-desktop -lv | grep single-workspace-mode | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $x -s "true"; done
-
-		while ! [[ $REPLY_GLOBAL == "NORMAL" || $REPLY_GLOBAL == "QUIT" ]]; do
-			sleep 0.01
-			if ! [ -d "/proc/${LIVE_WALLPAPER_PROGRAM_PID}" ]; then
-				REPLY_GLOBAL="NORMAL"
-				LIVE_WALLPAPER_PROGRAM_PID=0
-			fi
-			CURRENT_WORKSPACE=$( xdotool get_desktop )
+		while [[ $REPLY_GLOBAL == "NORMAL" ]]; do
+			sleep 0.006
 			DESKTOP_WAS_FOCUSED=$DESKTOP_FOCUSED
 			DESKTOP_FOCUSED=$( xprop -id $DESKTOP_WID | grep FOCUSED | wc -l )
 			if ((DESKTOP_WAS_FOCUSED==0 )); then
 				if (( DESKTOP_FOCUSED==1 )); then
-					sleep 0.85 #delay & check for flickering errors filtering
-					DESKTOP_FOCUSED=$( xprop -id $DESKTOP_WID | grep FOCUSED | wc -l )
-					if (( DESKTOP_FOCUSED==0 )); then
-						DESKTOP_WAS_FOCUSED=$DESKTOP_FOCUSED
-						continue
-					else
-						for ((i = 1 ; i <= $(wmctrl -l | wc -l) ; i++ )); do 
-							if(( $(wmctrl -l -x -G | awk 'NR=='$i'{print $2}') == CURRENT_WORKSPACE )); then
-								if(( $( xprop -id $(wmctrl -l -x -G | awk 'NR=='$i'{print $1}') | grep NET_WM_STATE_ABOVE | wc -l ) == 0 )); then
-									wmctrl -i -r $(wmctrl -l -x -G | awk 'NR=='$i'{print $1}') -b add,hidden 
-								fi
-							fi
-						done
-					fi
+					wmctrl -r "xfceliveDesktop" -b add,above
+					wmctrl -r "xfceliveDesktop" -b remove,below
 				fi
 			fi
+			if ((DESKTOP_WAS_FOCUSED==1 )); then
+				if (( DESKTOP_FOCUSED==0 )); then
+					wmctrl -r "xfceliveDesktop" -b remove,above
+					wmctrl -r "xfceliveDesktop" -b add,below
+					#wmctrl -r "xfceliveDesktop" -b remove,below
+				fi
+			fi
+			wait $( wmctrl -r "xfceliveDesktop" -e '0, 0, 0, 0, 0' )
+		done
+	elif ! [[ $REPLY_GLOBAL == "" ]]; then
+		LIVE_WALLPAPER
+		while ! [[ $REPLY_GLOBAL == "NORMAL" || $REPLY_GLOBAL == "QUIT" ]]; do
+			#sleep 0.005
+			sleep 0.006
+			COUNT+=1
+
+			if (( COUNT>=165 )); then
+
+				COUNT=0
+
+				if ! [ -d "/proc/${LIVE_WALLPAPER_PROGRAM_PID}" ]; then
+					REPLY_GLOBAL="NORMAL"
+					LIVE_WALLPAPER_PROGRAM_PID=0
+					continue
+				fi
+
+				if ! [[ $REPLY_GLOBAL == $LIVE_WALLPAPER_PROGRAM_PID ]]; then
+					LIVE_WALLPAPER_PROGRAM_PID=$REPLY_GLOBAL
+					NORMAL_WALLPAPER
+					LIVE_WALLPAPER
+					continue
+				fi
+			
+			fi
+			
+			DESKTOP_WAS_FOCUSED=$DESKTOP_FOCUSED
+			DESKTOP_FOCUSED=$( xprop -id $DESKTOP_WID | grep FOCUSED | wc -l )
+			LIVE_FOCUSED=$( xprop -id $LIVE_WALLPAPER_PROGRAM_WID | grep FOCUSED | wc -l )
+			if ((DESKTOP_WAS_FOCUSED==0 )); then
+				if (( DESKTOP_FOCUSED==1 )); then
+					wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,above
+					wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b remove,below
+					wmctrl -r "xfceliveDesktop" -b add,above
+					wmctrl -r "xfceliveDesktop" -b remove,below
+				fi
+			fi
+			if ((DESKTOP_WAS_FOCUSED==1 )); then
+				if (( DESKTOP_FOCUSED==0 )); then
+					wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b remove,above
+					wmctrl -i -r $LIVE_WALLPAPER_PROGRAM_WID -b add,below
+					wmctrl -r "xfceliveDesktop" -b remove,above
+					wmctrl -r "xfceliveDesktop" -b add,below
+				fi
+			fi
+			#if ((DESKTOP_WAS_FOCUSED==1 )); then
+				if (( LIVE_FOCUSED==1 )); then
+					wmctrl -a "xfceliveDesktop"
+				fi
+			#fi
+					#wmctrl -r "xfceliveDesktop" -b remove,below
 		wait $( wmctrl -r "xfceliveDesktop" -e '0, 0, 0, 0, 0' )
 		done
 	fi
